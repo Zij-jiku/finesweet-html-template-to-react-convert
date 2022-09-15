6303184a72cc7863eb6d3f9d3afdc89fb0a011f2
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-09-15 18:46:18 +0600

    slider-and-accodian-add

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,6 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './faq.css';
+
 function Faq() {
+    let [accodian , setAccodian] = useState([
+        {
+            title: "How much time does it take?1",
+            details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            status: true
+        },
+
+        {
+            title: "How much time does it take?2",
+            details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            status: false
+        },
+
+        {
+            title: "How much time does it take?3",
+            details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            status: false
+        },
+
+        {
+            title: "How much time does it take?4",
+            details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            status: false
+        },
+
+        {
+            title: "How much time does it take?5",
+            details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            status: false
+        },
+    ]);
+
+    let handleAccodian = (id) => {
+        console.log(id);
+
+        accodian.map((item,index) => {
+            if(id == index) {
+                item.status = true;
+                setAccodian([...accodian]);
+            }else {
+                item.status = false;  
+                setAccodian([...accodian]);
+            }
+        })
+    }
+
+
     return (
         <div>
             <section className="faq">
@@ -11,33 +59,17 @@ function Faq() {
                             <h6>Contact us for more info</h6>
                         </div>
                         <div className="faq_main">
-                            <div className="faq_item">
-                                <span>01</span>
-                                <h5>How much time does it take?</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                    labore et dolore magna aliqua.</p>
+
+                        {accodian.map((item, index) => (
+                            <div className="faq_item" onClick={() => handleAccodian(index)}>
+                                <span>{index +1 >= 10 ? index+1 : `0${index+1}`}</span>
+                                <h5>{item.title}</h5>
+                                {item.status && <p> {item.details} </p>}
                                 <i className="fa-solid fa-xmark"></i>
                             </div>
-                            <div className="faq_item">
-                                <span>02</span>
-                                <h5>What is your className naming convention?</h5>
-                                <i className="fa-solid fa-plus"></i>
-                            </div>
-                            <div className="faq_item">
-                                <span>03</span>
-                                <h5>How much time does it take?</h5>
-                                <i className="fa-solid fa-plus"></i>
-                            </div>
-                            <div className="faq_item">
-                                <span>04</span>
-                                <h5>How much time does it take?</h5>
-                                <i className="fa-solid fa-plus"></i>
-                            </div>
-                            <div className="faq_item">
-                                <span>05</span>
-                                <h5>How much time does it take?</h5>
-                                <i className="fa-solid fa-plus"></i>
-                            </div>
+                        ))}
+
+                            
                         </div>
                     </div>
                 </div>
--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -1,7 +1,67 @@
 import React from 'react'
 import './testimonial.css';
+import Slider from "react-slick";
+
+
+
+// Slider Arrow
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, 
+            width: "50px",
+            height: "50px",
+            lineHeight: "50px",
+            fontSize: "22px",
+            color: "#FFFFFF",
+            textAlign: "center",
+            background: "#2405F2",
+            borderRadius: "50%",
+            cursor: "pointer",
+            transition: ".5s"
+        }}
+            onClick={onClick}
+        >
+            <i className="fa-solid fa-angle-left"></i>
+        </div>
+    );
+}
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, 
+            width: "50px",
+            height: "50px",
+            lineHeight: "50px",
+            fontSize: "22px",
+            color: "#FFFFFF",
+            textAlign: "center",
+            background: "#2405F2",
+            borderRadius: "50%",
+            cursor: "pointer",
+            transition: ".5s",
+         }}
+            onClick={onClick}
+        >
+        <i className="fa-solid fa-angle-right"></i>
+        </div>
+    );
+}
 
 function Testimonial() {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
+
     return (
         <div>
             <section className="testimonial">
@@ -12,17 +72,45 @@ function Testimonial() {
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.</p>
                         </div>
                         <div className="testimonial_containt">
-                            <h4>"The best agency we’ve worked with so far. They understand our product and are able to add new
-                                features with a great
-                                focus."</h4>
-                            <div className="testimonial_user">
-                                <img src="images/testimonial.png" alt="testimonial" />
-                                    <h6>Jenny Wilson</h6>
-                                    <p>Vice President</p>
-                            </div>
-                            <div className="testimonial_arrow">
-                                <i className="fa-solid fa-angle-left"></i>
-                                <i className="fa-solid fa-angle-right"></i>
+                            <div>
+                                <Slider {...settings}>
+                                    <div>
+                                        <h4>"The best agency we’ve worked with so far. They understand our product and are able to add new
+                                            features with a great
+                                            focus."</h4>
+                                        <div className="testimonial_user">
+                                            <img src="images/testimonial.png" alt="testimonial" />
+                                            <h6>Jenny Wilson</h6>
+                                            <p>Vice President</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h4>"The best agency we’ve worked with so far. They understand our product and are able to add new
+                                            features with a great
+                                            focus."</h4>
+                                        <div className="testimonial_user">
+                                            <img src="images/testimonial.png" alt="testimonial" />
+                                            <h6>Jenny Wilson</h6>
+                                            <p>Vice President</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h4>"The best agency we’ve worked with so far. They understand our product and are able to add new
+                                            features with a great
+                                            focus."</h4>
+                                        <div className="testimonial_user">
+                                            <img src="images/testimonial.png" alt="testimonial" />
+                                            <h6>Jenny Wilson</h6>
+                                            <p>Vice President</p>
+                                        </div>
+                                    </div>
+
+                                </Slider>
+                                {/* <div className="testimonial_arrow">
+                                    
+                                    
+                                </div> */}
+
                             </div>
                         </div>
                     </div>
